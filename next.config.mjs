a1c6b7aa8f0d4cb5952b06071483472da74a4a32
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-05 03:58:44 +0200

    Update basePath in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/2048-in-react",
+    basePath: "/github-pages",
     output: "export",
     reactStrictMode: true,
 };
